eeb4fa750bcbc4f24c6ec4993230636947963b23
Daniel Brice <dbrice@cj.com>
2016-07-16 11:44:49 -0700

    added a test, but need to add scalatest to sbt

--- src/main/scala/FindLoop.scala
+++ src/main/scala/FindLoop.scala
@@ -1,24 +1,31 @@
+import FindLoop._
+import org.scalatest.FlatSpec
+
 object FindLoop {
 
   /**
-   * IV. FindLoop
-   *
-   * Given: a linked list of characters that is known to loop back on itself at
-   * some entry,
-   *
-   * When: your function is called on said list,
-   *
-   * Then: your function returns the node at which the list rejoins itself.
-   */
+    * IV. FindLoop
+    *
+    * Given: a linked list of characters that is known to loop back on itself at
+    * some entry,
+    *
+    * When: your function is called on said list,
+    *
+    * Then: your function returns the node at which the list rejoins itself.
+    */
 
-  type LoopNode = (Char, LoopNode)
-  type Loop = Seq[LoopNode]
+  case class ListNode(value : Char, next : ListNode)
 
   def findLoop
-    : Loop => LoopNode
-    = l    => ???
+  : ListNode => ListNode
+  = start    => ???
 }
 
-object FindLoopTests {
+object FindLoopTests extends FlatSpec {
 
+  "findLoop" should "return the first node in a two-node cycle" in {
+    // given: a two-node cycle
+    lazy val node1 = ListNode('A', node2)
+    lazy val node2 = ListNode('B', node1)
+  }
 }
