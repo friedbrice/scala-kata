afe2af066efbcc97a20c475160c646247a27febf
Daniel Brice <dbrice@cj.com>
2016-07-17 21:55:54 -0700

    added some tests

--- src/test/scala/net/danielbrice/scalakata/ValidateTree$Test.scala
+++ src/test/scala/net/danielbrice/scalakata/ValidateTree$Test.scala
@@ -16,11 +16,26 @@ class ValidateTree$Test extends FlatSpec with Matchers {
     result should be (true)
   }
 
-  "validateTree" should "???" in {
-    // given: ???
+  "validateTree" should "validate a one-node tree" in {
+    // given: a one-node tree
+    lazy val t = Branch[Int](1, Leaf, Leaf)
 
-    // when: ???
+    // when: validateTree is called
+    lazy val result = validateTree(t)
 
-    // then: ???
+    // then: true should be returned
+    result should be (true)
+  }
+
+  "validateTree" should "invalidate this awful tree" in {
+    // given: a horrible, invalid tree
+    lazy val t = Branch[Int](1, b, Leaf)
+    lazy val b = Branch[Int](2, Leaf, Leaf)
+
+    // when: validateTree is called
+    lazy val result = validateTree(t)
+
+    // then: false should be returned
+    result should be (false)
   }
 }
