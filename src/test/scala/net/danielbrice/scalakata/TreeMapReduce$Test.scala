00ae1605d9f80b883269682e6eff35faf51ac50f
Daniel Brice <dbrice@cj.com>
2016-07-19 19:45:56 -0700

    added tests

--- src/test/scala/net/danielbrice/scalakata/TreeMapReduce$Test.scala
+++ src/test/scala/net/danielbrice/scalakata/TreeMapReduce$Test.scala
@@ -5,5 +5,63 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class TreeMapReduce$Test extends FlatSpec with Matchers {
 
-  // TODO: Write some tests!
+  "treeMapReduce" should "return z when passed Leaf" in {
+    // given: some stubs
+    val f = (x : Any) => x
+    val g = (x: Any, y : Any) => y
+    val z = 0
+
+    // when: treeMapReduce is called on Leaf
+    lazy val result = treeMapReduce(f)(g)(z)(Leaf)
+
+    // then: result should be z
+    result should be (z)
+  }
+
+  "treeMapReduce" should "apply g" in {
+    // given: some stubs
+    lazy val f = (x : Int) => x
+    lazy val g = (x: Int, y : Int) => x + y
+    lazy val z = 0
+    lazy val b1 = Branch(1, b2, Leaf)
+    lazy val b2 = Branch(2, Leaf, Leaf)
+
+    // when: treeMapReduce is called
+    lazy val result = treeMapReduce(f)(g)(z)(b1)
+
+    // then: result should be z
+    result should be (3)
+  }
+
+  "treeMapReduce" should "apply f" in {
+    // given: some stubs
+    lazy val f = (x : Char) => x.asDigit
+    lazy val g = (x: Int, y : Int) => x + y
+    lazy val z = 0
+    lazy val b1 = Branch('1', b2, Leaf)
+    lazy val b2 = Branch('2', Leaf, Leaf)
+
+    // when: treeMapReduce is called
+    lazy val result = treeMapReduce(f)(g)(z)(b1)
+
+    // then: result should be z
+    result should be (3)
+  }
+
+  "treeMapReduce" should "preserve order" in {
+    // given: some stubs
+    lazy val f = (x : Char) => x.toString
+    lazy val g = (x: String, y : String) => x + y
+    lazy val z = ""
+    lazy val b1 = Branch('3', b2, b4)
+    lazy val b2 = Branch('2', b3, Leaf)
+    lazy val b3 = Branch('1', Leaf, Leaf)
+    lazy val b4 = Branch('4', Leaf, Leaf)
+
+    // when: treeMapReduce is called
+    lazy val result = treeMapReduce(f)(g)(z)(b1)
+
+    // then: result should be z
+    result should be ("1234")
+  }
 }
