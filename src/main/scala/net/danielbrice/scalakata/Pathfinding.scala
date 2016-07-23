569e9524dfdba6ffb8e9cfbcc192648401986135
Daniel Brice <dbrice@cj.com>
2016-07-23 11:10:19 -0700

    solved all the kata, just need to clean up tests for SayIt

--- src/main/scala/net/danielbrice/scalakata/Pathfinding.scala
+++ src/main/scala/net/danielbrice/scalakata/Pathfinding.scala
@@ -5,8 +5,9 @@ object Pathfinding {
   /**
    * VI. Pathfinding
    *
-   * Given: two nodes of a strongly-connected directed graph,
-   * (http://mathworld.wolfram.com/ConnectedDigraph.html)
+   * Given: two nodes of a strongly-connected directed graph
+   * (meaning, for any two nodes A and B, there is gauranteed
+   * to be a path from A to B),
    *
    * When: your function is called with said nodes,
    *
@@ -16,15 +17,49 @@ object Pathfinding {
   case class GraphNode(label : Char, neighbors : Unit => List[GraphNode])
 
   def pathfinding
-    : GraphNode => GraphNode => List[GraphNode]
-    = start     => end       => {
+  : GraphNode => GraphNode => List[GraphNode]
+  = start     => end       => {
 
-      def helper
-        : List[List[GraphNode]] => List[GraphNode] => List[GraphNode]
-        = paths                 => neighbors       => {
-          ???
-        }
+    type Path = List[GraphNode]
 
-      helper(List())(start.neighbors())
+    def step
+    // takes a path and extends it in all possible directions
+    : Path => List[Path]
+    = path => for {
+      next <- path.head.neighbors()
+    } yield next :: path
+
+    def steps
+    // takes a bunch of paths and extends each
+    // one in all of its possible directions
+    : List[Path] => List[Path]
+    = paths      => paths.flatMap(step)
+
+    def done
+    // takes a path and checks if it's complete
+    : Path => Boolean
+    = path => path.head.label == end.label
+
+    // the rest is basically Java, but with slightly nicer syntax
+    var pathsSoFar : List[Path] = List(List(start))
+    var currPaths : List[Path] = pathsSoFar
+    var currPath : Path = List()
+
+    var result : Path = List()
+
+    while (result.isEmpty) {
+
+      while (currPaths.nonEmpty && result.isEmpty) {
+        currPath = currPaths.head
+        currPaths = currPaths.tail
+
+        if (done(currPath)) result = currPath
+      }
+
+      pathsSoFar = steps(pathsSoFar)
+      currPaths = pathsSoFar
     }
+
+    result.reverse
+  }
 }
--- src/test/scala/net/danielbrice/scalakata/Pathfinding$Test.scala
+++ src/test/scala/net/danielbrice/scalakata/Pathfinding$Test.scala
@@ -27,4 +27,22 @@ class Pathfinding$Test extends FlatSpec with Matchers {
     // then: the result should be List(A)
     result should be (List(nodeA))
   }
+
+  "pathfinding" should "work" in {
+    // given: this graph
+    lazy val node1 : GraphNode = GraphNode('1', _ => List(node2, node5))
+    lazy val node2 : GraphNode = GraphNode('2', _ => List(node1, node3, node5))
+    lazy val node3 : GraphNode = GraphNode('3', _ => List(node2, node4))
+    lazy val node4 : GraphNode = GraphNode('4', _ => List(node3, node5, node6))
+    lazy val node5 : GraphNode = GraphNode('5', _ => List(node1, node2, node4))
+    lazy val node6 : GraphNode = GraphNode('6', _ => List(node4))
+
+    // when: pathfinding is called with node1 and node6
+    lazy val result = pathfinding(node1)(node6)
+
+    // then: the result should be
+    result.head should be (node1)
+    result.last should be (node6)
+    result.length should be (4)
+  }
 }
