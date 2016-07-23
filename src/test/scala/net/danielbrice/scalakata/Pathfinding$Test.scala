628a66236e26f6a3e41b76d0cafc9d29a17b77fc
Daniel Brice <dbrice@cj.com>
2016-07-22 19:58:52 -0700

    workinz

--- src/test/scala/net/danielbrice/scalakata/Pathfinding$Test.scala
+++ src/test/scala/net/danielbrice/scalakata/Pathfinding$Test.scala
@@ -5,26 +5,26 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Pathfinding$Test extends FlatSpec with Matchers {
 
-//  "pathfinding" should "return start when start == end" in {
-//    // given: a graph node A
-//    lazy val nodeA : GraphNode = new GraphNode('A', _ => List())
-//
-//    // when: pathfinding is called with node A and node A
-//    lazy val result = pathfinding(nodeA)(nodeA)
-//
-//    // then: the result should be List(A)
-//    result should be (List(nodeA))
-//  }
-//
-//  "pathfinding" should "find a minimal path" in {
-//    // given: a graph A->B->A
-//    lazy val nodeA : GraphNode = new GraphNode('A', _ => List(nodeB))
-//    lazy val nodeB : GraphNode = new GraphNode('A', _ => List(nodeA))
-//
-//    // when: pathfinding is called with node A and node A
-//    lazy val result = pathfinding(nodeA)(nodeA)
-//
-//    // then: the result should be List(A)
-//    result should be (List(nodeA))
-//  }
+  "pathfinding" should "return start when start == end" in {
+    // given: a graph node A
+    lazy val nodeA : GraphNode = new GraphNode('A', _ => List())
+
+    // when: pathfinding is called with node A and node A
+    lazy val result = pathfinding(nodeA)(nodeA)
+
+    // then: the result should be List(A)
+    result should be (List(nodeA))
+  }
+
+  "pathfinding" should "find a minimal path" in {
+    // given: a graph A->B->A
+    lazy val nodeA : GraphNode = new GraphNode('A', _ => List(nodeB))
+    lazy val nodeB : GraphNode = new GraphNode('A', _ => List(nodeA))
+
+    // when: pathfinding is called with node A and node A
+    lazy val result = pathfinding(nodeA)(nodeA)
+
+    // then: the result should be List(A)
+    result should be (List(nodeA))
+  }
 }
